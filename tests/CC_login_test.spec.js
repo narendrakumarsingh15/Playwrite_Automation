9da333731f42e5e86f0e419a8059b6afa90e93a9
uktstautomation02 <uktstautomation@michaelpage.com>
2025-09-03 05:37:16 +0000

    Added few test steps

--- tests/CC_login_test.spec.js
+++ tests/CC_login_test.spec.js
@@ -1,15 +1,18 @@
 const {test, expect} = require('playwright/test');
 
 test('Login to CC', async ({ page }) => {
-
 // 1. Go to login page
 await page.goto('https://pagegroup--sit.sandbox.my.salesforce.com/');
 const pageTitle= page.title();
 console.log('The page title is:', pageTitle);
 await expect(page).toHaveTitle('Login | Salesforce');
+});
+test('Enter Username and password', async ({ page }) => {
 // 2. Type username and password
 await page.fill('#username', 'qa.auto.sysadmin@michaelpage.com.sit');    // replace with actual selector
 await page.fill('#password', 'London2025$$$$$');    // replace with actual selector 
+});
+test('Clic on Login button', async ({ page }) => {
 // 3. Click login button
 await page.click('#Login');    // replace with actual selector  
 console.log('Login successful!');
